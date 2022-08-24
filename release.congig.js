798b5e3de147871511860e3a34d6365bb0b47580
Beatriz Miranda <beatriz.miranda@thoughtworks.com>
2022-08-24 11:18:47 -0300

    ci: create semantic commit

---
+++ release.congig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: 'main',
+  repositoryUrl: 'https://github.com/BeatrizMiranda/acolherse',
+  plugins: [
+    '@semantic-release/github',
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+  ]
+}
